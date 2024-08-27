87ac6422619d5667ebf15c9d8fdf938d9c6b31ef
pratham-sugandhi <psugandhi08@gmail.com>
2024-08-27 15:29:39 +0530

    yess

---
+++ JS-LEARNING/BUILDING-JS/sript.js
@@ -0,0 +1,27 @@
+// alert("hello world");
+
+// console.log("generating..");
+
+// let a =7;
+// let b=7;
+// let c;
+// console.log(c= a+b);
+
+// prompt=("enter a no");
+// let confirm=("blasting.., Enter a no");
+// if(isTrue){
+//     console.log(blasted)
+// }
+// else{
+//     console.log(d= "not blasting")
+// }
+
+
+// set-1
+
+let a=("number");
+let b=(5);
+let c;
+console.log(c=a+b);
+
+console.log(typeof c, typeof a);
