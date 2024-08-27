3fc20dd63da21088c186b31f86a49662892f2521
pratham-sugandhi <psugandhi08@gmail.com>
2024-08-27 19:15:16 +0530

    change

--- JS-LEARNING/BUILDING-JS/sript.js
+++ JS-LEARNING/BUILDING-JS/sript.js
@@ -24,4 +24,22 @@ let b=(5);
 let c;
 console.log(c=a+b);
 
-console.log(typeof c, typeof a);
+console.log(typeof c, typeof a, typeof b);
+
+const p={
+    "name":"pratham",
+    "class":12,
+}
+p.class="1 st year";  //we can not change value in const
+console.log(p);
+p.number=1;
+console.log(p);   //we can add new keys in object
+
+
+//js-dictionary
+const dict ={
+    p:"lorem5",
+    q:"lorem4",
+    r:"lorem2",
+};
+console.log(dict);
