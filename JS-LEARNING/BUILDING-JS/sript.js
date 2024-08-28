af6afdfbda55219a3b1563df18f71dd9473c4c0b
pratham-sugandhi <psugandhi08@gmail.com>
2024-08-28 13:45:54 +0530

    working..

--- JS-LEARNING/BUILDING-JS/sript.js
+++ JS-LEARNING/BUILDING-JS/sript.js
@@ -1,23 +1,25 @@
-// alert("hello world");
+//Day-1...
 
-// console.log("generating..");
+/*
+alert("hello world");
 
-// let a =7;
-// let b=7;
-// let c;
-// console.log(c= a+b);
+console.log("generating..");
 
-// prompt=("enter a no");
-// let confirm=("blasting.., Enter a no");
-// if(isTrue){
-//     console.log(blasted)
-// }
-// else{
-//     console.log(d= "not blasting")
-// }
+let a =7;
+let b=7;
+let c;
+console.log(c= a+b);
 
+prompt=("enter a no");
+let confirm=("blasting.., Enter a no");
+if(isTrue){
+    console.log(blasted)
+}
+else{
+    console.log(d= "not blasting")
+}
 
-// set-1
+*/
 
 let a=("number");
 let b=(5);
@@ -43,3 +45,42 @@ const dict ={
     r:"lorem2",
 };
 console.log(dict);
+
+//lec-2
+let age =5; 
+let grace=9;
+age+=grace;
+
+/*
+if (age>18){
+    console.log("you can drive")
+}
+else{
+    console.log("you can not drive")
+}
+console.log(age+grace);
+console.log(age);
+*/
+
+if (age>=18){
+    console.log("you can drive");
+}
+else if(age==0){
+    console.log("are you made");
+}
+else{
+    console.log("you can not drive");
+}
+console.log(age);
+
+let p=23;
+let l=22;
+let lp= l>p ?(l+p):(l-p);
+/*
+if(l>p){
+   console.log("lp"="l+p");
+}
+else{
+    console.log("lp"="l-p");
+}
+*/
