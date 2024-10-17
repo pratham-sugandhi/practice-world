8adae6f848f75970cd16020c443bead36da71efa
Pratham <pdsugndhi08@gmail.com>
2024-10-17 08:58:01 +0530

    staged

---
+++ HTMLCSS-PROJECT/JS-WORLD/index.js
@@ -0,0 +1,82 @@
+// console.log("Hello World");
+
+// OBJECT
+// const std1 ={
+//     name:"prathu",
+//     class:"it",
+//     sec:"A6"    
+// }
+// const std2 ={
+//     name:"laxuu",
+//     class:"it",
+//     sec:"A6"    
+// }
+// const std3 ={
+//     name:"pralakha",
+//     class:"it",
+//     sec:"A6"    
+// }
+
+// console.log("std1std2std3=" ,std1,std2,std3 );
+
+// V&D- practice-1
+// let a="prathu";
+// let b= `007`;
+
+// let c=a+b;
+// console.log(c);
+
+
+// let d="prathu";
+// let e= `007`;
+
+// let f=d+e;
+// console.log(f,typeof f);
+
+
+// const cls ={
+//     std :50,
+//     bys:29,
+//     gls:20,
+// }
+
+// const dict={
+//     pratham:"first",
+//     laxmi:"goodluck" 
+// }
+// console.log(dict);
+
+
+// const letter="d";
+// const newkey='dog';
+
+// const updatedDict={
+//     pratham:"first",
+//     laxmi:"goodluck",
+//     [letter]:"newkey"
+// };
+// console.log(updatedDict);
+
+// Operators-practice-2
+// let age1=16;
+// if(age1>=10 && age1<=20){
+//     console.log("Your age=",age1)
+// }
+// else{
+//     console.log("You are not elgible")
+// };
+
+// let age2=25;
+// if(age2>=10 && age2<=20){
+//     console.log("Your age=",age2)
+// }
+// else{
+//     console.log("You are not elgible")
+// }
+
+let num=4;
+for(num=4;num%2<=12;num++){
+    console.log("numbers are= ", num);
+}
+
+// // Faulty calci
