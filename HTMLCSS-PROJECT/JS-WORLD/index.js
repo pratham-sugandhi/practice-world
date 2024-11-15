be3a166e80a1a07637f8e0f771dc177378ff91b2
Pratham <pdsugndhi08@gmail.com>
2024-11-15 11:59:31 +0530

    DAY-1

--- HTMLCSS-PROJECT/JS-WORLD/index.js
+++ HTMLCSS-PROJECT/JS-WORLD/index.js
@@ -1,83 +1,187 @@
-// console.log("Hello World");
+{
+ // console.log("Hello World");
 
-// OBJECT
-// const std1 ={
-//     name:"prathu",
-//     class:"it",
-//     sec:"A6"    
+// // // OBJECT
+// // // const std1 ={
+// // //     name:"prathu",
+// // //     class:"it",
+// // //     sec:"A6"    
+// // // }
+// // // const std2 ={
+// // //     name:"laxuu",
+// // //     class:"it",
+// // //     sec:"A6"    
+// // // }
+// // // const std3 ={
+// // //     name:"pralakha",
+// // //     class:"it",
+// // //     sec:"A6"    
+// // // }
+
+// // // console.log("std1std2std3=" ,std1,std2,std3 );
+
+// // // V&D- practice-1
+// // // let a="prathu";
+// // // let b= `007`;
+
+// // // let c=a+b;
+// // // console.log(c);
+
+
+// // // let d="prathu";
+// // // let e= `007`;
+
+// // // let f=d+e;
+// // // console.log(f,typeof f);
+
+
+// // // const cls ={
+// // //     std :50,
+// // //     bys:29,
+// // //     gls:20,
+// // // }
+
+// // // const dict={
+// // //     pratham:"first",
+// // //     laxmi:"goodluck" 
+// // // }
+// // // console.log(dict);
+
+
+// // // const letter="d";
+// // // const newkey='dog';
+
+// // // const updatedDict={
+// // //     pratham:"first",
+// // //     laxmi:"goodluck",
+// // //     [letter]:"newkey"
+// // // };
+// // // console.log(updatedDict);
+
+// // // Operators-practice-2
+// // // let age1=16;
+// // // if(age1>=10 && age1<=20){
+// // //     console.log("Your age=",age1)
+// // // }
+// // // else{
+// // //     console.log("You are not elgible")
+// // // };
+
+// // // let age2=25;
+// // // if(age2>=10 && age2<=20){
+// // //     console.log("Your age=",age2)
+// // // }
+// // // else{
+// // //     console.log("You are not elgible")
+// // // }
+
+// // // let num=4;
+// // // for(num=4;num%2<=12;num++){
+// // //     console.log("numbers are= ", num);
+}
+
+
+
+// DAY-1;
+
+// { VARIABLES--
+// // console.log("prathu");
+
+// const accountId = 14445;
+// let accountEmail ="prathu@gmail.com";
+// var accountPass = "943";
+// accountCity = "jaipur";
+
+// // accountId = 23; 
+// accountEmail= "mm@gmail";
+// accountPass="678";
+// accountCity="bur";
+// // console.log(accountId);
+
+// console.table([accountId, accountEmail, accountPass, accountCity]);
+
+
+// // NEW TOPIC-- DATATYPES
+// "use strict"; // treat all js in newer ver
+
+// alert(3+3) -basically works in browser
+
+// let name = "prathu";   //string
+// let age = 19;  //num
+// let isLoggedIn = true; //boolean
+
 // }
-// const std2 ={
-//     name:"laxuu",
-//     class:"it",
-//     sec:"A6"    
-// }
-// const std3 ={
-//     name:"pralakha",
-//     class:"it",
-//     sec:"A6"    
-// }
 
-// console.log("std1std2std3=" ,std1,std2,std3 );
+// { DATATYPES--
+// // PRIMITIVE DATATYPES--
 
-// V&D- practice-1
-// let a="prathu";
-// let b= `007`;
-
-// let c=a+b;
-// console.log(c);
+// number => 2 to power 53;
+// bigInt {large numbers}
+// string => ""
+// boolean => true/false
+// null => type/ standalone value  //(empty)
+// undefined => value not provided
+// symbol => define comp/uniqueness
 
 
-// let d="prathu";
-// let e= `007`;
+// console.log(typeof null); // object 
+// console.log(typeof undefined); // undefined
 
-// let f=d+e;
-// console.log(f,typeof f);
-
-
-// const cls ={
-//     std :50,
-//     bys:29,
-//     gls:20,
+// // OBJECT DATATYPES--
+// null => object 
 // }
 
-// const dict={
-//     pratham:"first",
-//     laxmi:"goodluck" 
-// }
-// console.log(dict);
+// { DATATYPE CONVERSION--
 
+// let score = "33abc";
+// // console.log(typeof score);
 
-// const letter="d";
-// const newkey='dog';
+// let valueINum = Number(score);
+// // console.log(valueINum);
+// // console.log(typeof valueINum);
 
-// const updatedDict={
-//     pratham:"first",
-//     laxmi:"goodluck",
-//     [letter]:"newkey"
-// };
-// console.log(updatedDict);
+// // "33" => 33
+// // "33abc" => NaN  //typeof NaN-- number
+// // true => 1; false => 0;
 
-// Operators-practice-2
-// let age1=16;
-// if(age1>=10 && age1<=20){
-//     console.log("Your age=",age1)
-// }
-// else{
-//     console.log("You are not elgible")
-// };
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// // console.log(booleanIsLoggedIn);
 
-// let age2=25;
-// if(age2>=10 && age2<=20){
-//     console.log("Your age=",age2)
-// }
-// else{
-//     console.log("You are not elgible")
+// //  1 => true;  0 => false;
+// // "" => false; "prathu" => true;
+
+// let someNum = 33;
+// let stringNum = String(someNum);
+
+// // console.log(stringNum);
+// // console.log(typeof stringNum);
+
 // }
 
-// let num=4;
-// for(num=4;num%2<=12;num++){
-//     console.log("numbers are= ", num);
+// { OPERATIONS--
+
+// let value = 3;
+// let negValue = -value;
+// // console.log(negValue);
+
+// let str1 = "hello";
+// let str2 = "prathu";
+
+// let str3 = str1 + str2;
+// // console.log(str3);
+
+// // console.log( "2" + 2); //22
+// // console.log( 2 + "2");  //22
+// // console.log( "1"+ 2 +2);  //122
+// // console.log( 1 + 2 +"2");  //32
+
+// // console.log(+true); //1
+// // console.log(+"");  //0
+
+// let gameCounter = 100;
+// gameCounter++;  // ++gameCounter 
+// // console.log(gameCounter);
 // }
 
-// NEW WORK-- Building logics
-console.log("prathu")
+// DAY-2;
