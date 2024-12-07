3f8e036e26a9f3224c092867f96b8413e0e9756a
Pratham <pdsugndhi08@gmail.com>
2024-12-07 14:42:07 +0530

    projeects..

--- HTMLCSS-PROJECT/js-/script.js
+++ HTMLCSS-PROJECT/js-/script.js
@@ -463,7 +463,7 @@
 
 
 // CALCULATOR
-
+/*
 // const userInput = document.getElementById("userInput");
 let userInput = document.querySelector("#userInput");
 let expression = '';
@@ -482,3 +482,38 @@ function erase() {
     expression = '';
     userInput.value = expression;
 }
+*/
+
+
+
+// COLOR CHANGER
+const boxes = document.querySelectorAll('.box');
+console.log(boxes);
+
+const body =document.querySelector("body");
+
+boxes.forEach( function(box){
+    console.log(box);
+
+    box.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+        
+        if (e.target.id === 'gray'){
+            body.style.backgroundColor = e.target.id; //gray
+        }
+        if (e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id; //gray
+        }
+        if (e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id; //gray
+        }
+        if (e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id; //gray
+        }
+        if (e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id; //gray
+        }
+    });
+});
+
